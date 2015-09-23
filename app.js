b975c849020ba86cdd60591d2f5b4160b018553d
javakidx <javakidx@gmail.com>
2015-09-23 15:58:20 +0800

    modified

--- app.js
+++ app.js
@@ -4,10 +4,12 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-//var settings = require("./settings");
-var flsh = require("connect-flash");
+var settings = require("./settings");
+var flash = require("connect-flash");
 var routes = require('./routes/index');
 //var users = require('./routes/users');
+var session = require('express-session');
+var MongoStore = require('connect-mongo')(session);
 
 var app = express();
 
@@ -40,6 +42,17 @@ app.use(function(req, res, next) {
   err.status = 404;
   next(err);
 });
+
+app.use(session({
+  secret : settings.cookieSecret,
+  key: settings.db,
+  cookie : {maxAge: 1000 * 60 * 60 * 24 * 30},
+  store : new MongoStore({
+    db : settings.db,
+    host :settings.host,
+    port : settings.port
+  })
+}));
 
 // error handlers
 
